84b28c2ae5e9ffb39818b3d6f788d43c13250569
Pratik Magar <67797986+magarpratik@users.noreply.github.com>
2022-02-17 11:16:04 +0000

    fix problem with the comment

--- module5/module5_code.go
+++ module5/module5_code.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-// GetExampleDotCom uses the "net/http" package to send a GET request to example.com
+//GetExampleDotCom uses the "net/http" package to send a GET request to example.com
 
 func GetExampleDotCom() {
 	resp, err := http.Get("http://example.com/")
